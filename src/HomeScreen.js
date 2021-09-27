c69aaad292d75b5b926deef82432403543fb68d9
Aiden <aidenjiahao@gmail.com>
2021-09-27 19:30:16 -0400

    with errors version, the image couldnt get loaded

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -1,15 +1,28 @@
 import React from "react";
 import Nav from "./Nav";
 import Banner from "./Banner";
+import "./HomeScreen.css";
+import requests from "./Requests";
+import Row from "./Row";
 function HomeScreen() {
   return (
     <div className="homeScreen">
       {/* Nav(bar)*/}
       <Nav />
-
       {/* Banner*/}
       <Banner />
-      {/* Rows*/}
+      {/* Row*/}
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Top Rated" fetchUrl={requests.fetchToRated} />
+      <Row title="Actions Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
---
+++ src/Requests.js
@@ -0,0 +1,15 @@
+// Most of time we store in {process.env.API_KEY}
+const API_KEY = "14401cc53cb3af5f8fdab05dbb4a93e6";
+
+const requests = {
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=123`,
+  fetchToRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+};
+
+export default requests;
