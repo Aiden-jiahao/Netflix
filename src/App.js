9ce4129af05bfca82cd4cbd4fd1285b57dd24b13
Aiden <aidenjiahao@gmail.com>
2021-09-29 17:02:13 -0400

    it is working.

--- src/App.js
+++ src/App.js
@@ -4,20 +4,31 @@ import HomeScreen from "./screens/HomeScreen";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import LoginScreen from "./screens/LoginScreen";
 import { auth } from "./firebase";
+import { useDispatch, useSelector } from "react-redux";
+import { login, logout, selectUser } from "./features/userSlice";
+import ProfileScreen from "./screens/ProfileScreen";
 function App() {
-  const user = null;
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((userAuth) => {
       if (userAuth) {
         //Logged in
         console.log(userAuth);
+        dispatch(
+          login({
+            uid: userAuth.uid,
+            email: userAuth.email,
+          })
+        );
       } else {
         //Logged out
+        dispatch(logout());
       }
     });
     return unsubscribe;
-  }, []);
+  }, [dispatch]);
 
   return (
     <div className="app">
@@ -28,6 +39,10 @@ function App() {
           <Switch>
             <Route exact path="/">
               <HomeScreen />
+            </Route>
+
+            <Route path="/profile">
+              <ProfileScreen />
             </Route>
           </Switch>
         )}
