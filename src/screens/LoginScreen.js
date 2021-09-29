9cc09e7f467eb866ff64761bb37c12104364b1a4
Aiden <aidenjiahao@gmail.com>
2021-09-28 22:08:04 -0400

    until this beofre auth,  everything works

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "./LoginScreen.css";
+import SignupScreen from "./SignupScreen";
 function LoginScreen() {
   const [signIn, setSignIn] = useState(false);
 
@@ -19,7 +20,7 @@ function LoginScreen() {
 
       <div className="loginScreen__body">
         {signIn ? (
-          <SignInScreen />
+          <SignupScreen />
         ) : (
           <>
             <h1>Unlimited films, TV programmes and more. </h1>
