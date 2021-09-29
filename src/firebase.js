091752262deb1f0238a208072ab9590ca52c8fa4
Aiden <aidenjiahao@gmail.com>
2021-09-28 23:04:07 -0400

    this auth is working after deleting the node module and switch firebase to version 8.0.0

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,4 @@
+import firebase from "firebase";
 const firebaseConfig = {
   apiKey: "AIzaSyBFYPg7QUPD7w5ldmbvKDAUgjQoFE-FQcQ",
   authDomain: "netflix-3847d.firebaseapp.com",
@@ -6,3 +7,10 @@ const firebaseConfig = {
   messagingSenderId: "113690536099",
   appId: "1:113690536099:web:fef3bb184d8170540b3716",
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+const auth = firebase.auth();
+
+export { auth };
+export default db;
--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,18 +1,42 @@
-import React from "react";
+import React, { useRef } from "react";
+import { auth } from "../firebase";
 import "./SignupScreen.css";
 function SignupScreen() {
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
+
   const register = (e) => {
     e.preventDefault();
+    auth
+      .createUserWithEmailAndPassword(
+        emailRef.current.value,
+        passwordRef.current.value
+      )
+      .then((authUser) => {
+        console.log(authUser);
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
   };
   const signIn = (e) => {
     e.preventDefault();
+    auth
+      .signInWithEmailAndPassword(
+        emailRef.current.value,
+        passwordRef.current.value
+      )
+      .then((authUser) => {
+        console.log(authUser);
+      })
+      .catch((error) => alert(error.message));
   };
   return (
     <div className="signupScreen">
       <form>
         <h1>Sign In</h1>
-        <input placeholder="Email" type="email" />
-        <input placeholder="password" type="password" />
+        <input ref={emailRef} placeholder="Email" type="email" />
+        <input ref={passwordRef} placeholder="password" type="password" />
         <button onClick={signIn} type="submit">
           Sign In
         </button>
